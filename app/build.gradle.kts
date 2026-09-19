9a8af7b9d92d0347be3f78df796f938a905b11b9
za99405com-ui <za99405.com@gmail.com>
2026-09-20 02:12:06 +0300

    Release v1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.elmallah.paymentbridge"
     minSdk = 24
     targetSdk = 36
-    versionCode = 7
-    versionName = "1.3.0"
+    versionCode = 8
+    versionName = "1.3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
