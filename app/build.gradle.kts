4e2b89a30c7547e8dcfa3babfb84e77dac6d1c8d
za99405com-ui <za99405.com@gmail.com>
2026-09-19 09:40:54 +0300

    Bump payment bridge to v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.elmallah.paymentbridge"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.2.0"
+    versionCode = 4
+    versionName = "1.2.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
