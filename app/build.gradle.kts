466b1b79a433d057059a52f650b82c4c14abff2b
za99405com-ui <za99405.com@gmail.com>
2026-09-20 01:19:29 +0300

    Bump bridge to v1.3.0 for lifecycle integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.elmallah.paymentbridge"
     minSdk = 24
     targetSdk = 36
-    versionCode = 6
-    versionName = "1.2.3"
+    versionCode = 7
+    versionName = "1.3.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
